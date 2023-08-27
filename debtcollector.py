7377b5bc57b03d210edaf1d23e5a9597719f35f9
NNComputing <125534375+NNComputing@users.noreply.github.com>
2023-08-27 11:14:05 +0800

    Update debtcollector.py

--- debtcollector.py
+++ debtcollector.py
@@ -1,16 +1,17 @@
-# Debt Calculator for the interested debt collectors (feel free to change variable names and values to your preference)
+# Debt Calculator for the interested debt collectors
+# Feel free to change variable names and values to your preference
 
 def calculate_debt(a, b, c): # a, b and c are individual prices of certain things
     # add more arguments as necessary (if you need to work with more prices)
-    a_nums = int(input(f'How many times has your debtor not paid for something of price {a}?: ')) # number of times you paid price a
-    b_nums = int(input(f'How many times has your debtor not paid for something of price {b}?: ')) # number of times you paid price b
-    c_nums = int(input(f'How many times has your debtor not paid for something of price {c}?: ')) # number of times you paid price c
-    # remember to add d_nums, etc. in case you have more prices to work with
-    debt = (a_nums * a) + (b_nums * b) + (c_nums * c) # sum of debt (remember to add d_nums * d, etc. if you have more prices to work with)
-    print(f'The debt owed by this person is {debt}')
+    a_nums = int(input(f'How many times has your debtor not paid for something of price ${a:.2f}?: ')) # number of times you paid price a
+    b_nums = int(input(f'How many times has your debtor not paid for something of price ${b:.2f}?: ')) # number of times you paid price b
+    c_nums = int(input(f'How many times has your debtor not paid for something of price ${c:.2f}?: ')) # number of times you paid price c
+    # remember to add d_nums, etc. for more prices
+    debt = (a_nums * a) + (b_nums * b) + (c_nums * c) # sum of debt (remember to add d_nums, etc. for more prices)
+    print(f'The debt owed by this person is ${debt:.2f}')
 
 # main
-calculate_debt(3.40, 3.80, 1.00) # sample a,b and c: a = 3.40, b = 3.80, c = 1.00, a_nums = 10, b_nums = 6, c_nums = 11 
+calculate_debt(3.40, 3.80, 1.00) # sample values a = 3.40, b = 3.80, c = 1.00, a_nums = 10, b_nums = 6, c_nums = 11
 
 
 
